fec72a85dd2219a7bbea84a7c973880c976f3df9
swetshaw <swt.shw94@gmail.com>
2022-02-07 16:44:45 +0530

    added NFT creation and listing functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
-import {useNavigate,Link} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { ethers } from 'ethers';
 
-
-function Navbar() {  
+function Navbar() {
   const navigate = useNavigate();
 
   const [click, setClick] = useState(false);
@@ -14,36 +13,70 @@ function Navbar() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
   const [acc, setAcc] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    if (!isAuthenticated)  {
-      setIsAuthenticated(true)
-      console.log(isAuthenticated)
+    checkIfWalletIsConnected();
+    checkIfUserIsOnCorrectNetwork();
+  }, []);
+
+  const checkIfUserIsOnCorrectNetwork = async () => {
+    try {
+      const { ethereum } = window;
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log('Connected to chain ' + chainId);
+
+      const rinkebyChainId = '0x4';
+      if (chainId !== rinkebyChainId) {
+        alert('You are not connected to the Rinkeby Test Network!');
+      }
+    } catch (err) {
+      console.log(err);
     }
-  },[isAuthenticated]);
+  };
 
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
 
-  const connectWalletHandler = () => {
-		if (window.ethereum && window.ethereum.isMetaMask) {
+      if (!ethereum) {
+        alert('Get Metamask!');
+        return;
+      }
 
-			window.ethereum.request({ method: 'eth_requestAccounts'})
-			.then(result => {
-        let provider = ethers.getDefaultProvider();
-        // console.log(result[0],provider);
-        setAcc(result[0].slice(0,6)+'....'+result[0].slice(-5));
-        setIsAuthenticated(true)
-			})
-			.catch(error => {
-				alert("Error-"+error.message);
-			
-			});
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
 
-		} else {
-			console.log('Need to install MetaMask');
-			alert('Please install MetaMask browser extension to interact');
-		}
-	}
+      console.log('Connected', accounts[0]);
+      setAcc(accounts[0]);
+      setIsAuthenticated(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        setIsAuthenticated(false);
+        console.log('Make sure you have metamask!');
+      } else {
+        console.log('We have the ethereum object', ethereum);
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account', account);
+          setIsAuthenticated(true);
+          setAcc(account);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -58,88 +91,87 @@ function Navbar() {
     window.addEventListener('resize', showButton);
   }, []);
 
-
   return (
-      <IconContext.Provider value={{ color: '#d53f86' }}>
-        <nav class="flex sticky bg-[#0a111a] px-12 h-16 z-100 items-center justify-between text-[20px]">
-          <div className='flex items-center'>
-            <Link to='/' onClick={closeMobileMenu}>
-            <img className='rounded-md w-10 md:w-12' src={require('../assets/fandomDAO.png')} alt="Logo"/>
-            </Link>
-          </div>
+    <IconContext.Provider value={{ color: '#d53f86' }}>
+      <nav class='flex sticky bg-[#0a111a] px-12 h-16 z-100 items-center justify-between text-[20px]'>
+        <div className='flex items-center'>
+          <Link to='/' onClick={closeMobileMenu}>
+            <img
+              className='rounded-md w-10 md:w-12'
+              src={require('../assets/fandomDAO.png')}
+              alt='Logo'
+            />
+          </Link>
+        </div>
 
-          <div className='flex items-center h-16'>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-              <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
-                  <Link
-                    to='/'
-                    onClick={closeMobileMenu}
-                  >
-                    <p>Home</p>
-                  </Link>
-              </li>
+        <div className='flex items-center h-16'>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
+              <Link to='/' onClick={closeMobileMenu}>
+                <p>Home</p>
+              </Link>
+            </li>
 
-              <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
-                  <Link
-                    to='/about'
-                    onClick={closeMobileMenu}
-                  >
-                    <p>NFTs</p>
-                  </Link>
-              </li>
+            <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
+              <Link to='/about' onClick={closeMobileMenu}>
+                <p>NFTs</p>
+              </Link>
+            </li>
 
-              <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
-                  <Link
-                    to='/about'
-                    onClick={closeMobileMenu}
-                  >
-                    <p>About Us</p>
-                  </Link>
-              </li>
+            <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
+              <Link to='/about' onClick={closeMobileMenu}>
+                <p>About Us</p>
+              </Link>
+            </li>
 
-              <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
-                  <Link
-                    to='/contactus'
-                    onClick={closeMobileMenu}
-                  >
-                    <p>Contact Us</p>
-                  </Link>
-              </li>
-            </ul>
-          </div>
-          
-          {
-            isAuthenticated ? 
+            <li className='flex rounded-md text-[#d53f86] md:px-8 md:py-2 text-center md:no-underline md:items-center'>
+              <Link to='/contactus' onClick={closeMobileMenu}>
+                <p>Contact Us</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {isAuthenticated ? (
           <div className='flex'>
-            <img 
+            <img
               src={require('../assets/fandomDAO.png')}
               className='w-12 h-12 mr-2 rounded-full hidden md:flex cursor-pointer'
-              onClick={() => {navigate("/profile");}}
-              />
+              alt='nft'
+              onClick={() => {
+                navigate('/profile');
+              }}
+            />
             <div className='rounded-md bg-gradient-to-r from-[#df3f86] to-[#6218a8]'>
-              <button onClick={() => {navigate("/");}}
+              <button
+                onClick={() => {
+                  navigate('/');
+                }}
                 className='table text-white w-full p-2 md:px-2 md:py-2 text-center'
               >
                 {acc}
               </button>
             </div>
-          </div> :
-            <div className='flex text-white text-center'>
-              <button onClick={() => {connectWalletHandler();navigate("/editprofile");}}
-               className='table rounded-md bg-gradient-to-r from-[#df3f86] to-[#6218a8] text-white w-full p-2 md:px-2 md:py-2 text-center'
-              >
+          </div>
+        ) : (
+          <div className='flex text-white text-center'>
+            <button
+              onClick={() => {
+                connectWalletAction();
+                navigate('/editprofile');
+              }}
+              className='table rounded-md bg-gradient-to-r from-[#df3f86] to-[#6218a8] text-white w-full p-2 md:px-2 md:py-2 text-center'
+            >
               Connect Wallet
             </button>
           </div>
-          }
-          
+        )}
 
-          <div className='flex md:hidden' onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-          </div>
-
-        </nav>
-      </IconContext.Provider>
+        <div className='flex md:hidden' onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+      </nav>
+    </IconContext.Provider>
   );
 }
 
