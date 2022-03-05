104b79171077f6269afe20c3ec2176a82f5c8878
swetshaw <swt.shw94@gmail.com>
2022-03-05 23:48:34 +0530

    Cancel button fix

--- src/components/CreateNFT.js
+++ src/components/CreateNFT.js
@@ -331,7 +331,7 @@ const CreateNFT = ({ open, setOpen, acc }) => {
                 <button
                   type='button'
                   className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
-                  onClick={() => handleCancel}
+                  onClick={() => handleCancel()}
                   ref={cancelButtonRef}
                 >
                   Cancel
